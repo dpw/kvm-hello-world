cd225c1e342fb657beee318001e9f6187167a93b
David Wragg <david@wragg.org>
2016-06-21 06:43:59 +0100

    Support for non-paged 32-bit protected mode

--- kvm-hello-world.c
+++ kvm-hello-world.c
@@ -6,12 +6,13 @@
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <string.h>
+#include <stdint.h>
 #include <linux/kvm.h>
 
 struct vm {
 	int sys_fd;
 	int fd;
-	void *mem;
+	char *mem;
 };
 
 void vm_init(struct vm *vm, size_t mem_size)
@@ -97,7 +98,7 @@ void vcpu_init(struct vm *vm, struct vcpu *vcpu)
 	}
 }
 
-const unsigned char code[] = {
+const unsigned char code16[] = {
 	0xb0, 42, /* MOV AL, 42 */
 	0xf4, /* HLT */
 };
@@ -131,7 +132,7 @@ int run_real_mode(struct vm *vm, struct vcpu *vcpu)
 		exit(1);
 	}
 
-	memcpy(vm->mem, code, sizeof code);
+	memcpy(vm->mem, code16, sizeof code16);
 
 	if (ioctl(vcpu->fd, KVM_RUN, 0) < 0) {
 		perror("KVM_RUN");
@@ -156,12 +157,142 @@ int run_real_mode(struct vm *vm, struct vcpu *vcpu)
 	return res;
 }
 
-int main(void)
+void fill_segment_descriptor(uint64_t *dt, struct kvm_segment *seg)
+{
+	uint32_t limit = seg->g ? seg->limit >> 12 : seg->limit;
+	dt[seg->selector] = (limit & 0xffff) /* Limit bits 0:15 */
+		| (seg->base & 0xffffff) << 16 /* Base bits 0:23 */
+		| (uint64_t)seg->type << 40
+		| (uint64_t)seg->s << 44 /* system or code/data */
+		| (uint64_t)seg->dpl << 45 /* Privilege level */
+		| (uint64_t)seg->present << 47
+		| (limit & 0xf0000ULL) << 48 /* Limit bits 16:19 */
+		| (uint64_t)seg->avl << 52 /* Available for system software */
+		| (uint64_t)seg->l << 53 /* 64-bit code segment */
+		| (uint64_t)seg->db << 54 /* 16/32-bit segment */
+		| (uint64_t)seg->g << 55 /* 4KB granularity */
+		| (seg->base & 0xff000000ULL) << 56; /* Base bits 24:31 */
+}
+
+const unsigned char code32[] = {
+	0xb8, 42, 0, 0, 0, /* MOV EAX, 42 */
+	0xf4, /* HLT */
+};
+
+int run_prot32_mode(struct vm *vm, struct vcpu *vcpu)
+{
+	struct kvm_segment seg = {
+		.base = 0,
+		.limit = 0xffffffff,
+		.selector = 1,
+		.present = 1,
+		.dpl = 0,
+		.db = 1,
+		.s = 1, /* Code/data */
+		.l = 0,
+		.g = 1, /* 4KB granularity */
+	};
+
+	struct kvm_sregs sregs;
+	struct kvm_regs regs;
+	int res;
+	uint64_t *gdt;
+
+        if (ioctl(vcpu->fd, KVM_GET_SREGS, &sregs) < 0) {
+		perror("KVM_GET_SREGS");
+		exit(1);
+	}
+
+	sregs.cr0 |= 1; /* set PE: enter protected mode */
+	sregs.gdt.base = 4096;
+	sregs.gdt.limit = 3 * 8 - 1;
+
+	gdt = (void *)(vm->mem + sregs.gdt.base);
+	/* gdt[0] is the null segment */
+
+	seg.type = 11; /* Code: execute, read, accessed */
+	seg.selector = 1;
+	fill_segment_descriptor(gdt, &seg);
+	sregs.cs = seg;
+
+	seg.type = 3; /* Data: read/write, accessed */
+	seg.selector = 2;
+	fill_segment_descriptor(gdt, &seg);
+	sregs.cs = sregs.ds = sregs.es = sregs.fs = sregs.gs = sregs.ss = seg;
+
+        if (ioctl(vcpu->fd, KVM_SET_SREGS, &sregs) < 0) {
+		perror("KVM_SET_SREGS");
+		exit(1);
+	}
+
+	memset(&regs, 0, sizeof(regs));
+	/* Clear all FLAGS bits, except bit 1 which is always set. */
+	regs.rflags = 2;
+	regs.rip = 0;
+
+	if (ioctl(vcpu->fd, KVM_SET_REGS, &regs) < 0) {
+		perror("KVM_SET_REGS");
+		exit(1);
+	}
+
+	memcpy(vm->mem, code32, sizeof code32);
+
+	if (ioctl(vcpu->fd, KVM_RUN, 0) < 0) {
+		perror("KVM_RUN");
+		exit(1);
+	}
+
+	if (vcpu->kvm_run->exit_reason != KVM_EXIT_HLT) {
+		fprintf(stderr,
+			"Got exit_reason %d, expected KVM_EXIT_HLT (%d)\n",
+			vcpu->kvm_run->exit_reason, KVM_EXIT_HLT);
+		exit(1);
+	}
+
+	if (ioctl(vcpu->fd, KVM_GET_REGS, &regs) < 0) {
+		perror("KVM_GET_REGS");
+		exit(1);
+	}
+
+	res = (regs.rax == 42);
+	printf("RAX = %lld: %s\n", regs.rax,
+	       res ? "OK" : "wrong");
+	return res;
+}
+
+int main(int argc, char **argv)
 {
 	struct vm vm;
 	struct vcpu vcpu;
+	enum { REAL_MODE, PROT32_MODE } mode = REAL_MODE;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "rs")) != -1) {
+		switch (opt) {
+		case 'r':
+			mode = REAL_MODE;
+			break;
+
+		case 's':
+			mode = PROT32_MODE;
+			break;
+
+		default:
+			fprintf(stderr, "Usage: %s [ -r | -s ]\n", argv[0]);
+			return 1;
+		}
+	}
 
 	vm_init(&vm, 0x100000);
 	vcpu_init(&vm, &vcpu);
-	return !run_real_mode(&vm, &vcpu);
+
+	switch (mode) {
+	case REAL_MODE:
+		return !run_real_mode(&vm, &vcpu);
+
+	case PROT32_MODE:
+		return !run_prot32_mode(&vm, &vcpu);
+	}
+
+	return 1;
 }
