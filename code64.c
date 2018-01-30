9b3c6e337b1d2cedeb7b45870344afdc6c7da8ae
Ben Noordhuis <info@bnoordhuis.nl>
2018-01-30 01:07:46 +0100

    Add port I/O hypercall example.
    Implement QEMU's magic port 0xE9 and use it to print a "hello world"
    message from the guest.

--- code64.c
+++ code64.c
@@ -1,10 +1,17 @@
 #include <stddef.h>
 #include <stdint.h>
 
+static void outb(uint16_t port, uint8_t value) {
+	asm("outb %0,%1" : /* empty */ : "r" (value), "Nd" (port) : "memory");
+}
+
 void
 __attribute__((noreturn))
 __attribute__((section(".start")))
 start(void) {
+	for (const char *p = "Hello, 64-bit world!\n"; *p; ++p)
+		outb(0xE9, *p);
+
 	*(uint64_t *) 0x400 = 42;
 
 	for (;;)
